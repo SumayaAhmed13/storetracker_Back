90956dd7e7948c5e9f0b3f54fe959a94103e9919
SumayaShemo <sumayaahmedbd@gmail.com>
2024-05-30 16:54:43 +0600

    Basic Setup

---
+++ StoreApi/StoreApi/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApi.Data;
+using StoreApi.Entities;
+
+namespace StoreApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext _context)
+        {
+            context = _context;
+        }
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+            
+        }
+
+        [HttpGet("{id}")]
+        [MapToApiVersion("1.0")]
+        public async Task< ActionResult<Product>>GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);      
+           
+        }
+    }
+}
---
+++ StoreApi/StoreApi/Program.cs
@@ -0,0 +1,56 @@
+using Asp.Versioning;
+using Microsoft.EntityFrameworkCore;
+using StoreApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+});
+//For Api Versioning
+builder.Services.AddApiVersioning(opt =>
+{
+    opt.AssumeDefaultVersionWhenUnspecified = true; //This ensures if client doesn't specify an API version. The default version should be considered. 
+    opt.DefaultApiVersion = new ApiVersion(1, 0); //This we set the default API version
+    opt.ReportApiVersions = true; //The allow the API Version information to be reported in the client  in the response header. This will be useful for the client to understand the version of the API they are interacting with.
+}).AddApiExplorer(options => {
+        options.GroupNameFormat = "'v'VVV"; //The say our format of our version number “‘v’major[.minor][-status]”
+        options.SubstituteApiVersionInUrl = true; //This will help us to resolve the ambiguity when there is a routing conflict due to routing template one or more end points are same.
+    }); 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+var scope=app.Services.CreateScope();
+var context=scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initializ(context);
+
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "A problem occured during migration");
+}
+
+app.Run();
